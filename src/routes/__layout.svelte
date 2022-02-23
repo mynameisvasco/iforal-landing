687a4ab9ef9a94cde31d7f8002bd563424a99650
Vasco Sousa <myemailvasco@gmail.com>
2022-02-23 16:54:11 +0000

    :tada: Config change

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,6 @@
 	import { page } from '$app/stores';
 	import Footer from '$lib/footer/footer.svelte';
 	import Navbar from '$lib/navbar/navbar.svelte';
-	import '../app.scss';
 	import '../scss/bootstrap-theme.scss';
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -5,26 +5,13 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			scss: {
-				prependData: '@use "src/variables.scss" as *;'
-			}
-		})
-	],
+	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter()
 
-		vite: {
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: '@use "src/variables.scss" as *;'
-					}
-				}
-			}
-		}
+		// hydrate the <div id="svelte"> element in src/app.html
+		// target: '#svelte'
 	}
 };
 
